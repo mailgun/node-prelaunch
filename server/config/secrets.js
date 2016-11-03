3a96f010c18886da3110280949effdbbbe27d43c
jaimeagudo <jaimeagudo@users.no.reply.github.com>
2016-11-03 14:30:56 +0100

    fix mongodb_uri

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: process.env.NODE_ENV || 'development',
   appName: process.env.APP_NAME || 'Node Prelaunch',
-  db: process.env.MONGODB || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/node-prelaunch',
+  db: process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/node-prelaunch',
   mailgun: {
     domain: process.env.MAILGUN_DOMAIN || '',
     api: process.env.MAILGUN_API_KEY || '',
